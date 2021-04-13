20721aa4dde83769ecb9fb1f9d86a2b7d7cafce4
DevMunesh <81867465+DevMunesh@users.noreply.github.com>
2021-04-13 23:53:00 +0530

    Update CreateLog.php

--- PriceChangeLog/Observer/CreateLog.php
+++ PriceChangeLog/Observer/CreateLog.php
@@ -2,7 +2,6 @@
 namespace DevMunesh\PriceChangeLog\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\Event\Observer;
 use Magento\Backend\Model\Auth\Session;
 use Magento\Framework\Stdlib\DateTime\DateTime;
@@ -11,19 +10,16 @@ use DevMunesh\PriceChangeLog\Logger\Logger;
 
 class CreateLog implements ObserverInterface
 {
-    protected $_productRepository;
     protected $_logger;
     protected $_authSession;
     protected $_date;
     
     public function __construct(
-        ProductRepository $productRepository,
         Logger $logger,
         Session $session,
         DateTime $date
     )
     {
-        $this->_productRepository = $productRepository;
         $this->_logger = $logger;
         $this->_authSession = $session;
         $this->_date = $date;
@@ -34,13 +30,10 @@ class CreateLog implements ObserverInterface
         // get Product from observer
         $product = $observer->getProduct();
 
-        // get Product from table
-        $product_db = $this->_productRepository->getById($product->getEntityId());
-
         // price comparison
         // if not equal -> create a log
-        if($product->getPrice() != $product_db->getPrice()){
-            $outputLog = "Price changed for product::  ID: " . $product->getEntityId() . "  SKU: " . $product->getSku() . "\n\t Last Price: " . $product_db->getPrice() . "  New Price: " . $product->getPrice() . "\n\t User: " . $this->getCurrentUser() . "\n\t Updation Time: " . $this->getCurrentTime();
+        if($product->getFinalPrice() != $product->getOrigData('price')){
+            $outputLog = "Price changed for product::  ID: " . $product->getEntityId() . "  SKU: " . $product->getSku() . "\n\t Last Price: " . $product->getOrigData('price') . "  New Price: " . $product->getFinalPrice() . "\n\t User: " . $this->getCurrentUser() . "\n\t Updation Time: " . $this->getCurrentTime();
             
             // creates log
             $this->_logger->info($outputLog);
